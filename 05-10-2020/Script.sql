db2cad3ee799f17b01d355ee1b3742552ebc75ff
serosh k vikraman <serosh.vikraman@outlook.com>
2020-10-05 15:31:02 +0530

    Script

---
+++ 05-10-2020/Script.sql
@@ -0,0 +1,33 @@
+ALTER TABLE Project
+DROP CONSTRAINT FK_Project_BillingTypesCode;
+
+ALTER TABLE Project
+DROP CONSTRAINT FK_Project_BUCategoryCode;
+
+ALTER TABLE Project
+DROP CONSTRAINT FK_Project_ContractStatusCode;
+
+ALTER TABLE Project
+DROP CONSTRAINT FK_Project_ContractTypeCode;
+
+ALTER TABLE Project
+DROP CONSTRAINT FK_Project_CountryCode;
+
+ALTER TABLE Project
+DROP CONSTRAINT FK_Project_GroupingParametersCode;
+
+ALTER TABLE Project
+DROP CONSTRAINT FK_Project_ManagementCategoryCode;
+
+ALTER TABLE Project
+DROP CONSTRAINT FK_Project_ProjectEntityCode;
+
+ALTER TABLE Project
+DROP CONSTRAINT FK_Project_ProjectSegmentCode;
+
+ALTER TABLE Project
+DROP CONSTRAINT FK_Project_SmartViewCode;
+
+ALTER TABLE Project
+DROP CONSTRAINT FK_Project_StatutoryCategoryCode;
+
