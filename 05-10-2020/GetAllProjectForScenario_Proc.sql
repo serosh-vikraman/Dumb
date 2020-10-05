cb4e3d985b88f56a60f65c3c7c34000d19be2c76
serosh k vikraman <serosh.vikraman@outlook.com>
2020-10-05 15:36:58 +0530

    scru

--- 05-10-2020/GetAllProjectForScenario_Proc.sql
+++ 05-10-2020/GetAllProjectForScenario_Proc.sql
@@ -1,32 +1,4 @@
  
-ALTER PROCEDURE [dbo].[LinkProject] @P_Id int,@P_IFSProjectCode varchar(20),@P_CreatedBy varchar(50)
-
-	
-AS
-BEGIN
-
-Declare @V_ProjectName varchar(200);
-
-
-
-Select @V_ProjectName = project_short_title from meref_temp_projects where project_code = @P_IFSProjectCode;
-
-UPDATE [dbo].[Project]
-   SET [ProjectType] = 'Referential'
-      ,[IFSProjectCode] = @P_IFSProjectCode
-	  ,[ProjectName]=@V_ProjectName
-      ,[UpdatedBy] = @P_CreatedBy
-      ,[UpdatedAt] = getdate()
- WHERE ProjectID = @P_Id; 
-
- 
- Select @P_Id;
- 
-END;
-
-
-
-
 ALTER PROCEDURE [dbo].[GetAllProjectForScenario] @P_ScenarioID int
 	 
 AS
