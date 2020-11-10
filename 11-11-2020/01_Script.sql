7903b576191578d8445e9fa46f665f90b08831de
serosh k vikraman <serosh.vikraman@outlook.com>
2020-11-11 00:26:28 +0530

    aaa

---
+++ 11-11-2020/01_Script.sql
@@ -0,0 +1,8 @@
+-- Alter Scripts
+
+
+alter table ScenarioFiles add  [Year] int not null default 0,[Quarter] varchar(15)  null 
+
+
+
+alter table ScenarioUploadLog add ColumnNumber int not null default 0
