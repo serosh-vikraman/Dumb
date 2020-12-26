30805365a337c00d1a7a9cd9fbdd0db322f2581f
serosh k vikraman <serosh.vikraman@outlook.com>
2020-12-27 00:09:15 +0530

    aa

---
+++ 26-12-2020/LockQuarter2010.sql
@@ -0,0 +1,73 @@
+
+INSERT INTO [dbo].[LockQuarter]
+           ([Year]
+           ,[Quarter]
+           ,[Lock]
+           ,[CreatedBy]
+           ,[CreatedAt])
+     VALUES
+           (2010
+           ,'Q1'
+           ,1
+           ,1
+           ,getdate()
+           )
+
+    INSERT INTO [dbo].[LockQuarter]
+           ([Year]
+           ,[Quarter]
+           ,[Lock]
+           ,[CreatedBy]
+           ,[CreatedAt])
+     VALUES
+           (2010
+           ,'Q2'
+           ,1
+           ,1
+           ,getdate()
+           )
+
+		   INSERT INTO [dbo].[LockQuarter]
+           ([Year]
+           ,[Quarter]
+           ,[Lock]
+           ,[CreatedBy]
+           ,[CreatedAt])
+     VALUES
+           (2010
+           ,'Q3'
+           ,1
+           ,1
+           ,getdate()
+           )
+	INSERT INTO [dbo].[LockQuarter]
+           ([Year]
+           ,[Quarter]
+           ,[Lock]
+           ,[CreatedBy]
+           ,[CreatedAt])
+     VALUES
+           (2010
+           ,'Q4'
+           ,1
+           ,1
+           ,getdate()
+           )
+
+
+    INSERT INTO [dbo].[LockYear]
+           ([Year]  
+		   ,[Lock]
+           ,[CreatedBy]
+           ,[CreatedAt]
+           )
+     VALUES
+           (2010
+		   ,1
+           ,1
+           ,getdate()
+           );
+		   
+
+
+
